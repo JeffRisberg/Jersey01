57d43b45f092e532c68426d8f88c6719f3ee9f30
JeffRisberg <jrisberg@aol.com>
2017-10-28 17:35:33 -0700

    updated Guice registration

--- src/main/java/com/incra/jersey01/MainModule.java
+++ src/main/java/com/incra/jersey01/MainModule.java
@@ -1,7 +1,6 @@
 package com.incra.jersey01;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
 import com.incra.jersey01.controllers.CharityController;
 import com.incra.jersey01.controllers.DonorController;
 import com.incra.jersey01.services.CharityService;
@@ -26,7 +25,9 @@ public class MainModule extends AbstractModule {
     public List<Class<?>> resources() {
         return Arrays.asList(
                 CharityController.class,
-                DonorController.class
+                DonorController.class,
+                CharityService.class,
+                DonorService.class
         );
     }
 }
