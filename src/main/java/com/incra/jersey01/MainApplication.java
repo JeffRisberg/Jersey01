73227ffb7b23f6d1a267c7232ffa37d0cb1f7b00
JeffRisberg <jrisberg@aol.com>
2017-11-06 12:43:39 -0800

    added gzipping of results

--- src/main/java/com/incra/jersey01/MainApplication.java
+++ src/main/java/com/incra/jersey01/MainApplication.java
@@ -2,7 +2,9 @@ package com.incra.jersey01;
 
 import com.google.inject.Guice;
 import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.jersey.message.GZipEncoder;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.filter.EncodingFilter;
 import org.jvnet.hk2.guice.bridge.api.GuiceBridge;
 import org.jvnet.hk2.guice.bridge.api.GuiceIntoHK2Bridge;
 
@@ -17,5 +19,7 @@ public class MainApplication extends ResourceConfig {
         GuiceBridge.getGuiceBridge().initializeGuiceBridge(serviceLocator);
         GuiceIntoHK2Bridge guiceBridge = serviceLocator.getService(GuiceIntoHK2Bridge.class);
         guiceBridge.bridgeGuiceInjector(Guice.createInjector(new MainModule()));
+
+        EncodingFilter.enableFor(this, GZipEncoder.class);
     }
 }
